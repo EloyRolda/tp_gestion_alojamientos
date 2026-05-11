6b847e4905f00c93813335a177e0549d15f70ca9
EloyRolda <eloyrolda@gmail.com>
2026-05-11 14:52:17 -0300

    Project_Push
    Recuerden colocar la contraseña de su propio sv en el properties

---
+++ GA/src/main/java/GestionAlojamiento/GA/GaApplication.java
@@ -0,0 +1,13 @@
+package GestionAlojamiento.GA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaApplication.class, args);
+	}
+
+}
