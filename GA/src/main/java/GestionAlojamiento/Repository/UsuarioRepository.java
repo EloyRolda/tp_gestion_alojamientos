5666aa1da8c547f166be7d3339c9d0f810b6f95c
Maximo <maximoluzkevich@gmail.com>
2026-05-25 18:41:46 -0300

    agrege tymeleaf, agrege buscar por mail, cuidado la contraseña del application.properties

--- GA/src/main/java/GestionAlojamiento/Repository/UsuarioRepository.java
+++ GA/src/main/java/GestionAlojamiento/Repository/UsuarioRepository.java
@@ -3,9 +3,12 @@ package GestionAlojamiento.Repository;
 import GestionAlojamiento.Model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     /// Verifica si existe el correco en la db
     boolean existsByEmail(String email);
 
+    Optional<Usuario> findByEmail(String email);
 
 }
