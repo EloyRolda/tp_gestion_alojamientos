7ea51340a65a2b1f36b7a734a0906e44666a540b
EloyRolda <eloyrolda@gmail.com>
2026-05-29 23:22:16 -0300

    v0.6351 [Logic update]
    Solo letras que puse en mayusculas.

--- GA/src/main/java/GestionAlojamiento/RestController/ClienteController.java
+++ GA/src/main/java/GestionAlojamiento/RestController/ClienteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/Cliente")
 public class ClienteController {
 
     private final ClienteService clienteService;
--- GA/src/main/java/GestionAlojamiento/RestController/ReservaController.java
+++ GA/src/main/java/GestionAlojamiento/RestController/ReservaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/reserva")
+@RequestMapping("/Reserva")
 public class ReservaController {
     private final ReservaService reservaService;
 
